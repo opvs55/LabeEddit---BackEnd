b4240baa6f993cec9e0d5e3240009a95835c1d3d
Samuel Muniz ~~ (Samvs Magnvm) <97551057+opvs55@users.noreply.github.com>
2023-03-13 15:03:17 -0300

    Update PostRouter.ts

--- src/Router/PostRouter.ts
+++ src/Router/PostRouter.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import { PostBusiness } from "../Business/PostBusiness"
 import { PostController } from "../Controller/PostController"
-import { PostDataBase } from "../Database/PostDatabase"
+import { PostDataBase } from "../Database/PostDataBase"
 import { IdGenerator } from "../Services/IdGenerator"
 import { TokenManager } from "../Services/TokenManager"
 
